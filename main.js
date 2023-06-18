3fe8e0a4915b30dca46b0cb3faa0aab0a51a8bbb
Steven <134742313+StevenH0824@users.noreply.github.com>
2023-06-18 17:15:59 -0400

    added flexbox and background function to grid
    used google fonts and styled the page to look a bit more presentable as
    a chessboard.

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ characters should form a chessboard using console.log
 When program generates this pattern " #" than define a binding size and change 
 the program so that it works for any size. 
 */ 
-let size = 0;
+let size = 8;
 let adjusting = true;
 let pattern =" #";
 
-pattern = generateSize(8);
-displayGrid(pattern);
+//pattern = generateSize(8);
+//displayGrid(pattern);
 
 function generateSize(size="8",pattern= " #"){
     let adjusting = true;
@@ -33,3 +33,112 @@ function displayGrid(pattern){
         }
     }
 }
+
+
+// chessboard
+let column = document.querySelector('.main-Container');
+function createGrid(size=8){
+    let classname = ".column-";
+    // Creates the columns for the grid
+    for (let i=1; i <= size; i++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('grid');
+        newDiv.classList.add(classname.slice(1,-1)); //column
+        newDiv.classList.add(classname.slice(1)+i);
+        column.appendChild(newDiv);
+    }
+    // Creates the row in each column
+    for (let i = 1; i <= size; i++){
+        let column = document.querySelector(classname+i);
+        for(let k=1; k <= size; k++){
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('grid');
+            newDiv.classList.add(`${i}${k}`); // I think i can complete it with this value
+            newDiv.classList.add('row-'+k);
+            column.appendChild(newDiv);
+        }
+    }
+}
+
+
+function setBGColor(color1='#F3DEBA',color2='#ABC4AA'){
+    let grids = document.querySelectorAll('.grid');
+
+    grids.forEach((grid) => {
+        //grid.style.backgroundColor = "white";
+        // It fucking works, LETS GOOOOO
+        //if (grid.classList.contains('88')){
+        //    grid.style.backgroundColor = "gray";
+        //}
+        /*
+        for (let column = 1; column <= 8; column++){
+            if (grid.classList.contains(`${1}${column}`)){
+                if (column%2 != 0){
+                    grid.style.backgroundColor = color1;
+                }
+            }
+        }
+        */
+        for (let column = 1; column <= 8; column++){
+            for (let row = 1; row <= 8; row++){
+                if (grid.classList.contains(`${column}${row}`)){
+                    if ( (column % 2) != 0 && (row % 2) != 0){
+                        grid.style.backgroundColor = color1;
+                    } else if (column %2 == 0 && row % 2 == 0){
+                        grid.style.backgroundColor = color1;
+                    } else{
+                        grid.style.backgroundColor = color2;
+                    }
+
+                }
+            }
+        }
+
+    
+
+
+
+    });
+
+}
+
+ 
+
+
+createGrid();
+setBGColor();
+
+
+
+
+/*
+Because I'm scared as shit of starting over.
+
+function setBGColor(color1='#F3DEBA',color2='#ABC4AA'){
+    let grids = document.querySelectorAll('.grid');
+
+    grids.forEach((grid) => {
+        //grid.style.backgroundColor = "white";
+        // It fucking works, LETS GOOOOO
+        //if (grid.classList.contains('88')){
+        //    grid.style.backgroundColor = "gray";
+        //}
+        for (let column = 1; column <= 8; column++){
+            for (let row = 1; row <= 8; row++){
+                if (grid.classList.contains(`${column}${row}`)){
+
+
+                    
+                    if (column % 2 == 0 && (row % 2) == 0){
+                        grid.style.backgroundColor = color1;
+                    } else {
+                        grid.style.backgroundColor = color2;
+                    }
+                }
+            }
+        }
+    });
+
+}
+
+*/
